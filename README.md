9f96241d1da2f5394e79a452f939a67051d7d9b9
L-Chris <563303226@qq.com>
2018-10-25 18:13:43 +0800

    release

--- README.md
+++ README.md
@@ -3,7 +3,20 @@ vuex plugin: store vue-router history for better controlling
 
 ## Useage
 ```javascript
-import { link } from 'vuex-router-history'
+import r from '@/router'
+import store from '@/store'
+import { sync } from 'vuex-router-history'
 
-link(store, router, options)
+const router = sync(store, r, options)
+
+new Vue({
+  el: '#app',
+  render: () => <App/>
+  router,
+  store
+})
+
+
+this.$store.history.stack // history stack
+this.$store.history.current // current route
 ```
--- src/index.js
+++ src/index.js
@@ -1,52 +1,91 @@
-import { createProxy } from './utils'
+import createProxy from 'create-proxy'
 
-export function link (store, router, options) {
+const ROUTE_CHANGED = 'ROUTE_CHANGED'
+const STACK_CHANGED_PUSH = 'STACK_CHANGED_PUSH'
+const STACK_CHANGED_REPLACE = 'STACK_CHANGED_REPLACE'
+const STACK_CHANGED_GO = 'STACK_CHANGED_GO'
+const STACK_CHANGED_BACK = 'STACK_CHANGED_BACK'
+const STACK_CHANGED_FORWARD = 'STACK_CHANGED_FORWARD'
+
+export function sync (store, router, options) {
   const moduleName = (options || {}).moduleName || 'history'
   const currentRoute = cloneRoute(router.currentRoute)
+
+  const getMutationName = name => `${moduleName}/${name}`
+
+  const handleRouterGo = (state, n) => {
+    const targetIndex = state.index + n
+    if (targetIndex < 0 || targetIndex >= this.stack.length) return
+    const route = state.stack[targetIndex]
+    state.current = route
+  }
 
   store.registerModule(moduleName, {
     namespaced: true,
     state: {
       stack: [currentRoute],
-      current: currentRoute
+      current: currentRoute,
+      index: -1
     },
     mutations: {
-      'ROUTE_CHANGED' (state, transition) {
-        state[moduleName].current = cloneRoute(transition.to, transition.from)
+      [ROUTE_CHANGED] (state, { to, from }) {
+        state.current = cloneRoute(to, from)
+      },
+      [STACK_CHANGED_PUSH] (state, route) {
+        state.stack.push(route)
+        state.index++
+      },
+      [STACK_CHANGED_REPLACE] (state, route) {
+        state.stack = state.stack.slice(0, state.index).concat(route)
+      },
+      [STACK_CHANGED_GO] (state, [n]) {
+        handleRouterGo(state, n)
+      },
+      [STACK_CHANGED_BACK] (state) {
+        handleRouterGo(state, -1)
+      },
+      [STACK_CHANGED_FORWARD] (state) {
+        handleRouterGo(state, 1)
       }
     }
   })
 
   // sync store on router navigation
   router.afterEach((to, from) => {
-    currentPath = to.fullPath
     store.commit(moduleName + '/ROUTE_CHANGED', { to, from })
   })
 
   const pRouter = createProxy(router, {
     push: {
-      apply (target, thisArg, args) {
-        target.apply(thisArg, args)
+      apply (target, thisArg, [route]) {
+        target.apply(thisArg, [route, () => {
+          store.commit(getMutationName(STACK_CHANGED_PUSH), route)
+        }])
       }
     },
     replace: {
-      apply (target, thisArg, args) {
-        target.apply(thisArg, args)
+      apply (target, thisArg, [route]) {
+        target.apply(thisArg, [route, () => {
+          store.commit(getMutationName(STACK_CHANGED_REPLACE), route)
+        }])
       }
     },
     go: {
       apply (target, thisArg, args) {
         target.apply(thisArg, args)
+        store.commit(getMutationName(STACK_CHANGED_GO), args)
       }
     },
     back: {
       apply (target, thisArg, args) {
         target.apply(thisArg, args)
+        store.commit(getMutationName(STACK_CHANGED_BACK))
       }
     },
     forward: {
       apply (target, thisArg, args) {
         target.apply(thisArg, args)
+        store.commit(getMutationName(STACK_CHANGED_FORWARD))
       }
     }
   })
